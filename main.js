75e76af91d9bd91eb27f74411f3727e476d00edc
ptadil <ptadil72@gmail.com>
2024-12-31 19:51:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,150 @@
+let boxx5cartIcon = document.querySelector ("#add-cart5");
+let boxx5cart = document.querySelector(".boxx5");
+let boxx5closecart = document.querySelector("#boxxclose-cart5");
+
+boxx5cartIcon.onclick = () => {
+    boxx5cart.classList.add('active');
+};
+
+boxx5closecart.onclick = () => {
+    boxx5cart.classList.remove('active');
+};
+
+let boxx6cartIcon = document.querySelector ("#add-cart6");
+let boxx6cart = document.querySelector(".boxx6");
+let boxx6closecart = document.querySelector("#boxxclose-cart6");
+
+boxx6cartIcon.onclick = () => {
+    boxx6cart.classList.add('active');
+};
+
+boxx6closecart.onclick = () => {
+    boxx6cart.classList.remove('active');
+};
+
+let boxx7cartIcon = document.querySelector ("#add-cart7");
+let boxx7cart = document.querySelector(".boxx7");
+let boxx7closecart = document.querySelector("#boxxclose-cart7");
+
+boxx7cartIcon.onclick = () => {
+    boxx7cart.classList.add('active');
+};
+
+boxx7closecart.onclick = () => {
+    boxx7cart.classList.remove('active');
+};
+let boxx8cartIcon = document.querySelector ("#add-cart8");
+let boxx8cart = document.querySelector(".boxx8");
+let boxx8closecart = document.querySelector("#boxxclose-cart8");
+
+boxx8cartIcon.onclick = () => {
+    boxx8cart.classList.add('active');
+};
+
+boxx8closecart.onclick = () => {
+    boxx8cart.classList.remove('active');
+};
+
+
+let boxx11cartIcon = document.querySelector ("#add-cart11");
+let boxx11cart = document.querySelector(".boxx11");
+let boxx11closecart = document.querySelector("#boxxclose-cart11");
+
+boxx11cartIcon.onclick = () => {
+    boxx11cart.classList.add('active');
+};
+
+boxx11closecart.onclick = () => {
+    boxx11cart.classList.remove('active');
+};
+
+
+
+
+
+
+
+
+
+let cartIcon = document.querySelector ("#card-icon");
+let cart = document.querySelector(".cart");
+let closecart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+    cart.classList.add('active');
+};
+
+closecart.onclick = () => {
+    cart.classList.remove('active');
+};
+
+
+let boxxcartIcon = document.querySelector ("#add-cart1");
+let boxxcart = document.querySelector(".boxx");
+let boxxclosecart = document.querySelector("#boxxclose-cart1");
+
+boxxcartIcon.onclick = () => {
+    boxxcart.classList.add('active');
+};
+
+boxxclosecart.onclick = () => {
+    boxxcart.classList.remove('active');
+};
+
+
+
+
+
+
+let a3= document.querySelector ("#add-cart3");
+let b3 = document.querySelector(".boxx3");
+let c3 = document.querySelector("#boxxclose-cart3");
+
+a3.onclick = () => {
+    b3.classList.add('active');
+};
+
+c3.onclick = () => {
+    b3.classList.remove('active');
+};
+
+
+
+let boxx4cartIcon = document.querySelector ("#add-cart4");
+let boxx4cart = document.querySelector(".boxx4");
+let boxx4closecart = document.querySelector("#boxxclose-cart4");
+
+boxx4cartIcon.onclick = () => {
+    boxx4cart.classList.add('active');
+};
+
+boxx4closecart.onclick = () => {
+    boxx4cart.classList.remove('active');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
